4edbc6c31c2d1118b88d970bcd839de39afa76a5
EtienneL321 <etienne.lagace@shaw.ca>
2025-01-17 16:22:11 -0800

    Moving hashtable implementation to it's own repo

--- AdventOfCode2024/main.c
+++ AdventOfCode2024/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 #include "helper.h"
-#include "hashtable.h"
 
 int calculate_distance(int *list1, int *list2, int size)
 {
@@ -41,61 +40,5 @@ int main()
 
   printf("The total distance between both lists is %d\n", distance);
 
-  // for (int i = 0; i < size; i++)
-  // {
-  //   printf("Values at %d is: %d and %d\n", i, list1[i], list2[i]);
-  // }
-
-  Hashtable map;
-
-  initialize_hashtable(&map);
-
-  add_to_hashtable(&map, "sport");
-  add_to_hashtable(&map, "Sport");
-  add_to_hashtable(&map, "sport");
-  add_to_hashtable(&map, "train");
-  add_to_hashtable(&map, "Hello World!");
-  add_to_hashtable(&map, "Hello World!");
-  add_to_hashtable(&map, "Hello World!");
-  add_to_hashtable(&map, "Hello World!");
-  add_to_hashtable(&map, "Hello World!");
-  add_to_hashtable(&map, "Hello World!");
-  add_to_hashtable(&map, "Hello World");
-
-  print_hashtable(&map);
-
-  char *key = "Hello World!";
-  int n = search_hashtable(&map, key);
-  if (n != -1)
-  {
-    printf("Key \"%s\" has a value of %d\n", key, n);
-  }
-
-  /**
-   * An intersting bug occurs here where just inputing the string "Hello World" as the
-   * input causes a "No key found error" even if the key "Hello World" can be found.
-   * This is probably caused by a key of type char * being different than the default type
-   * given to the c string "Hello World".
-   */
-  int c = search_hashtable(&map, (char *)"Hello world");
-
-  key = "sport";
-  remove_from_hashtable(&map, key);
-  // print_hashtable(&map);
-
-  add_to_hashtable(&map, "tree");
-  add_to_hashtable(&map, "Francais");
-  add_to_hashtable(&map, "Fourmi");
-  add_to_hashtable(&map, "Joe Dassin");
-  add_to_hashtable(&map, "Duran Duran");
-  add_to_hashtable(&map, "Queen");
-  add_to_hashtable(&map, "View to a Kill");
-  // add_to_hashtable(&map, "Rio");
-  // add_to_hashtable(&map, "Hungry");
-
-  print_hashtable(&map);
-
-  free_hashtable(&map);
-  printf("\n");
   return 0;
 }
--- advent_of_code.sh
+++ advent_of_code.sh
@@ -15,8 +15,7 @@ if [ "$language" == "-p" ]; then
 elif [ "$language" == "-c" ]; then
   path="./AdventOfCode$year/main.c"
   helper_path="./AdventOfCode$year/helper.c"
-  hashtable_path="./AdventOfCode$year/hashtable.c"
-  gcc $path $helper_path $hashtable_path -o build/main
+  gcc $path $helper_path -o build/main
 
   # run c executable
   ./build/main
