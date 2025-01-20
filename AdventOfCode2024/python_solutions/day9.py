6bf87dc25c3b056d93cef8613c2dfc4764490ee9
EtienneL321 <etienne.lagace@shaw.ca>
2025-01-19 19:16:33 -0800

    Added partially working hard solution to day 9

--- AdventOfCode2024/python_solutions/day9.py
+++ AdventOfCode2024/python_solutions/day9.py
@@ -9,6 +9,9 @@ def day_9(input_text):
   checksum = read_by_line(input_text, calculate_filesystem_checksum)
   print(f"The checksum of the first filesystem is {checksum}\n")
 
+  checksum = read_by_line(input_text, calculate_filesystem_checksum_without_fragmentation)
+  print(f"The checksum of filesystem without fragmentation is {checksum}\n")
+
 ##
 # unravel_disk_map unravels the code it is given into a series of file space and free space
 #
@@ -66,7 +69,15 @@ def move_blocks(disk_map):
     
   return disk_map
   
-
+##
+# calculate_filesystem_checksum calculates the checksum of the filesystem
+#
+# Variables:
+# - line is the input line that will be processed
+#
+# Returns:
+# - checksum is the checksum of the filesystem
+##
 def calculate_filesystem_checksum(line):
   disk_map = unravel_disk_map(line)
 
@@ -77,5 +88,63 @@ def calculate_filesystem_checksum(line):
   while disk_map[i] != ".":
     checksum += (i * int(disk_map[i]))
     i += 1 
+  
+  return checksum
+
+
+def add_metadata_to_list(line):
+  temp_list = []
+  for n in line:
+    if n != "\n":
+      temp_list.append(int(n))
+
+  i = 0
+  while i < len(temp_list):
+    if i % 2 == 0:
+      temp_list[i] = (temp_list[i], i // 2)
+    else:
+      temp_list[i] = (temp_list[i], ".")
+    i += 1
+
+  return temp_list
+
+def move_blocks_without_fragmentation(line):
+  line = add_metadata_to_list(line)
+  right = len(line) - 1
+
+  while right > 1:
+    left = 1
+    while left < right:
+
+      if line[left][0] >= line[right][0]:
+
+        # if statements change the template for the disk map
+        if right == len(line) - 1:
+          line = line[:left] + [(0, ".")] + [line[right]] + [(line[left][0] - line[right][0], ".")] + line[left+1:right - 1]
+        else:
+          line = line[:left] + [(0, ".")] + [line[right]] + [(line[left][0] - line[right][0], ".")] + line[left+1:right - 1] + [(line[right - 1][0] + line[right][0] + line[right + 1][0], ".")] + line[right + 2:]
+
+        # print(line)
+        right += 2
+        break
+      else:
+        left += 2
+
+    right -= 2
+  
+  return line
+
+
+def calculate_filesystem_checksum_without_fragmentation(line):
+  line = move_blocks_without_fragmentation(line)
+
+  checksum = 0
+  i = 0
+  for pair in line:
+    n = pair[0]
+    for _ in range(n):
+      if pair[1] != ".":
+        checksum += (i * pair[1])
+      i += 1
   
   return checksum
