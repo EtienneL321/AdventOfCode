6408794a70f8862045cf9648c575a3495ddb6f2c
EtienneL321 <etienne.lagace@shaw.ca>
2025-01-23 16:23:14 -0800

    Added hard solution to day 10

--- AdventOfCode2024/python_solutions/day10.py
+++ AdventOfCode2024/python_solutions/day10.py
@@ -10,6 +10,9 @@ def day_10(input_text):
   trailhead_sum = calculate_trailhead_scores(matrix)
   print(f"Trailhead score sum is: {trailhead_sum}")
 
+  trailhead_sum = calculate_trailhead_rating(matrix)
+  print(f"Trailhead score sum is: {trailhead_sum}")
+
 
 def inbounds(matrix, i, j):
   return i >= 0 and i < len(matrix) and j >= 0 and j < len(matrix[0])
@@ -52,5 +55,43 @@ def calculate_trailhead_scores(matrix):
     for j in range(len(matrix[0])):
       if matrix[i][j] == 0:
         total_count += bfs(matrix, i, j)
+
+  return total_count
+
+
+def bfs_without_visited(matrix, i, j):
+  queue = [(i, j)]
+  trailhead_count = 0
+  directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+  while queue:
+    i, j = queue.pop(0)
+    height = matrix[i][j]
+
+    for x, y in directions:
+      new_i = i + x
+      new_j = j + y
+
+      if inbounds(matrix, new_i, new_j):
+        if matrix[new_i][new_j] == height + 1:
+          if matrix[new_i][new_j] == 9:
+            trailhead_count += 1
+          else:
+            queue.append((new_i, new_j))
+
+  return trailhead_count
+
+
+def calculate_trailhead_rating(matrix):
+  # First loop should go through every position in the matrix to find a 0
+  # When a 0 is found, begin a breadth first search that will add to the queue everytime the new position is an increment of 1 from the current position
+    # Don't forget to store previous positions in a set to prevent dulicates
+  # If a nine is reached, add it to the trailheads count
+  
+  total_count = 0
+  for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+      if matrix[i][j] == 0:
+        total_count += bfs_without_visited(matrix, i, j)
 
   return total_count
