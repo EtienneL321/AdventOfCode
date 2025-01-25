8b33205e8f7267ba9c06b984ebb3ee18210ba818
EtienneL321 <etienne.lagace@shaw.ca>
2025-01-24 17:02:14 -0800

    Added guidelines for day 11 hard solution

--- AdventOfCode2024/python_solutions/day11.py
+++ AdventOfCode2024/python_solutions/day11.py
@@ -9,6 +9,45 @@ def day_11(input_text):
   print("************************* Day 11 *************************")
   print("**********************************************************")
 
+  # I need to use some dynamic programming to solve for more than 40 iterations
+  """
+  What i have pictures in my head looks like so
+  0 -> 1 (1st) -> 2024 (2nd) -> 20 24 (3rd) -> 2 0 2 4 (4th) -> 4048 1 4048 8096 (5th) ...
+
+  With each iteration, a dictionary is filled up with information on how many nodes are present at each iteration
+  (1st iteration)
+  0: [1]
+
+  (2nd iteration)
+  0: [1, 1]
+  1: [1]
+
+  (3rd iteration) 
+  
+  0: [1, 1, 2]
+  1: [1, 2]
+  2024: [2]
+
+  (4th iteration)
+  0: [1, 1, 2, 4]
+  1: [1, 2, 4]
+  2024: [2, 4]
+  20: [2]
+  24: [2]
+
+  (5th iteration)
+  0: [1, 1, 2, 4, 4]
+  1: [1, 2, 4, 4]
+  2024: [2, 4, 4]
+  20: [2, 4]
+  24: [2, 4]
+  2: [1]
+  4: [1]
+
+  Once we get to this stage, 0 is reused. We check to see if the numbers of iterations for 0 (currently size 5) is
+  greater than or equal to iterations - current iteration (5). If it is, then we can return the number of nodes at
+  that iteration. If not, we add to the iteration node count. (still not 100% sure how this part will be done yet)
+  """
   number_of_stones = read_by_line(input_text, get_stone_count)
   print(f"The number of stones after {iterations} iterations is: {number_of_stones}")
 
