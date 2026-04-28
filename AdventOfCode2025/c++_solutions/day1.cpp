040c8774687d06277a0d762bdc4976ad4ef0e5be
EtienneL321 <etienne.lagace@shaw.ca>
2026-04-27 17:15:37 -0700

    feat: add solution for 2025 day 1 part 2

--- AdventOfCode2025/c++_solutions/day1.cpp
+++ AdventOfCode2025/c++_solutions/day1.cpp
@@ -28,6 +28,7 @@ void calculateCombination(std::string);
 int startPosition = 50;
 int position = startPosition;
 int zeroPositionOccurences = 0;
+int zeroExtraPositionOccurences = 0;
 
 void day_1()
 {
@@ -38,18 +39,22 @@ void day_1()
   std::string test_file = "./AdventOfCode2025/puzzle_inputs/day_1_test_input.txt";
   std::string real_file = "./AdventOfCode2025/puzzle_inputs/day_1_input.txt";
 
-  InputReader puzzle(test_file);
+  InputReader puzzle(real_file);
 
   position = startPosition;
   puzzle.ReadByLine(calculateCombination);
 
-  std::cout << "The number of 0 occurences in the sequence is " << zeroPositionOccurences << "." << std::endl;
+  std::cout << "The number of 0 occurences in the sequence is " << zeroPositionOccurences << std::endl;
+  std::cout << "The number of 0 occurences using method 0x434C49434B is " << zeroExtraPositionOccurences << std::endl;
 }
 
 void calculateCombination(std::string movement)
 {
+  // std::cout << "movement: " << movement << "  " << position << "  ";
   char move = movement[0];
   int steps = std::stoi(movement.substr(1));
+
+  int prevPos = position;
 
   if (move == 'L')
   {
@@ -60,11 +65,22 @@ void calculateCombination(std::string movement)
     position += steps;
   }
 
+  zeroExtraPositionOccurences += std::abs(position / 100);
+
+  if (position <= 0 && prevPos != 0)
+  {
+    zeroExtraPositionOccurences += 1;
+  }
+
+  // std::cout << "zero occurences: " << zeroExtraPositionOccurences << "  ";
+
   // We use 100 instead of 99 because our range is 0-99 (100 integers)
-  position %= 100;
+  position = (position % 100 + 100) % 100;
 
   if (position == 0)
   {
     zeroPositionOccurences++;
   }
+
+  // std::cout << position << std::endl;
 }
