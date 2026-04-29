d06ffc19e503af1a3172a9ea0110a955b76faa3f
EtienneL321 <etienne.lagace@shaw.ca>
2026-04-29 12:17:18 -0700

    feat: add partial 2025 c++ day 2 part 2 solution

--- AdventOfCode2025/c++_solutions/day2.cpp
+++ AdventOfCode2025/c++_solutions/day2.cpp
@@ -6,6 +6,7 @@
 
 void invalidId(std::string);
 bool repeatedSequence(std::string);
+bool repeatedXSequence(std::string);
 
 // class InvalidIdPuzzle : public InputReader
 // {
@@ -28,6 +29,7 @@ bool repeatedSequence(std::string);
 // };
 
 long long sumOfInvalidIds = 0;
+long long sumOfInvalidXIds = 0;
 
 void day_2(std::string fileName)
 {
@@ -40,8 +42,8 @@ void day_2(std::string fileName)
 
   puzzle.readBySeparator(separator, invalidId);
 
-  std::cout << "The sum of invalid ids is " << sumOfInvalidIds << std::endl;
-  // std::cout << "The number of 0 occurences using method 0x434C49434B is " << zeroExtraPositionOccurences << std::endl;
+  std::cout << "The sum of invalid ids with twice repeated patterns is " << sumOfInvalidIds << std::endl;
+  std::cout << "The sum of invalid ids with multiple repeated patterms is " << sumOfInvalidXIds << std::endl;
 }
 
 void invalidId(std::string ids)
@@ -62,10 +64,19 @@ void invalidId(std::string ids)
   while (start <= end)
   {
     std::string id = std::to_string(start);
+    // First solution
     if (id.length() % 2 == 0 && repeatedSequence(id))
     {
       sumOfInvalidIds += std::stoll(id);
     }
+
+    // Second solution
+    if (repeatedXSequence(id))
+    {
+      std::cout << id << std::endl;
+      sumOfInvalidXIds += std::stoll(id);
+    }
+
     start++;
   }
 }
@@ -84,4 +95,47 @@ bool repeatedSequence(std::string id)
     mid++;
   }
   return true;
+}
+
+bool repeatedXSequence(std::string id)
+{
+  if (id.length() == 1)
+  {
+    // id cannot have a repeated sequence
+    return false;
+  }
+
+  // Create substrings until halfway through
+  std::string sub = std::to_string(id[0]); // first substring to test
+  int mid = id.length() / 2;               // middle index of id
+
+  while (sub.length() <= mid)
+  {
+    // Step 1: Check if id is divisible by substring length
+    if (id.length() % sub.length() != 0)
+    {
+      sub += id[sub.length() + 1];
+      continue;
+    }
+
+    // Step 2: Check if this substring creates a pattern
+    // First loop jumps every substring length
+    for (int i = 0; i < id.length(); i += sub.length())
+    {
+      // Second loop checks that the current string matches the substring
+      for (int j = i; j < i + sub.length(); j++)
+      {
+        // No match means the substring is not repeated
+        if (id[j] != sub[j - i])
+        {
+          continue;
+        }
+      }
+    }
+
+    // Step 3: If we made it past the double for loop, that means out substring was repeated
+    return true;
+  }
+
+  return false;
 }
